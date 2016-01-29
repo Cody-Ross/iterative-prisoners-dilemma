033abd586f603efaa997defeb4f6e63dba3aa661
Cody Ross <Cody.888826970@roughers.net>
2016-01-29 12:33:48 -0600

    NewNEw

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -417,7 +417,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 12:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'CodyRiley'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
